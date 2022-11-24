c5ea8eaf4387a3773fa5eeec4d0d2282b4431be9
shashank <srshahare22@gmail.com>
2022-11-24 14:01:14 +0530

    added env file

--- config/db.config.js
+++ config/db.config.js
@@ -2,23 +2,19 @@ require("dotenv").config();
 const Sequelize = require("sequelize");
 
 let sequelize;
-if (process.env.QOVERY_APPLICATION_Z36BEDFA6_ENVIRONMENT_NAME === "production") {
-  sequelize = new Sequelize(process.env.QOVERY_POSTGRESQL_ZE91675C1_DATABASE_URL)
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_DATABASE,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      HOST: process.env.DB_HOST,
-      PORT: process.env.DB_PORT,
-      dialect: "postgres",
-      operatorsAliases: false,
-      logging: false,
-      ssl: true
-      // operatorsAliases: 0,
-    }
-  );
-}
+sequelize = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    HOST: process.env.DB_HOST,
+    PORT: process.env.DB_PORT,
+    dialect: "postgres",
+    operatorsAliases: false,
+    logging: false,
+    ssl: true
+    // operatorsAliases: 0,
+  }
+);
 
 module.exports = sequelize;
